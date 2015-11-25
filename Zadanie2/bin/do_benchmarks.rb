b046803f549fe5da5d08da965c1bbd904a778d3e
ltarnowski1 <ltarnowski@sigma.ug.edu.pl>
2015-11-25 19:19:38 +0100

    Update

--- Zadanie2/bin/do_benchmarks.rb
+++ Zadanie2/bin/do_benchmarks.rb
@@ -3,12 +3,13 @@ require_relative '../lib/quickSort'
 require_relative '../lib/insertSort'
 require 'benchmark'
 
-array = (1..1000).map { rand }
-#array2 = (1..10000).map { rand }
 
-Benchmark.bmbm do |x|
+array = (1..1000).map { rand }
+
+Benchmark.bm(4) do |x|
   x.report("sort! 1000") { array.dup.sort! }
   x.report("insert_sort! 1000") { insert_sort!(array) }
   x.report("bubble_sort! 1000") { bubble_sort!(array) }
   x.report("quick_sort! 1000") { quick_sort!(array) }
 end
+
